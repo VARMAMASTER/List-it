f3bcba5c382c14f3676f04bbc3f564a29e3a128f
VARMAMASTER <118650885+VARMAMASTER@users.noreply.github.com>
2023-11-03 11:07:02 +0530

    New folder structure is Created

---
+++ Backend/Database/authData.js
@@ -0,0 +1,31 @@
+const auth = [
+    {
+        id : 789653214,
+        name:"varma",
+        email:"sai.varma@gmail.com",
+        password:"123654789",
+        image: "https//static/images/varma"
+    },
+
+    {
+        id : 78965314,
+        name:"sai kiran varma",
+        email:"sai.kiran@gmail.com",
+        password:"123654789"
+    },
+
+    {
+        id : 7896532,
+        name:"sai varma",
+        email:"sai@gmail.com",
+        password:"123654789"
+    },
+
+    {
+        id : 78965324,
+        name:"kiran varma",
+        email:"kiran@gmail.com",
+        password:"123654789"
+    }
+
+];
---
+++ Backend/Database/userData.js
@@ -0,0 +1,25 @@
+const userData = [
+  {
+    id: "some id",
+    Todos: [
+      {
+        date: "2015/5/15",
+        Title: "some title",
+        todo: "some todo",
+      },
+    ],
+  },
+
+  {
+    id: "some id",
+    Todos: [
+      {
+        id: 459785456,
+        date: "2015/5/15",
+        Title: "some title",
+        todo: "some todo",
+        color:"pink"
+      },
+    ],
+  },
+];
